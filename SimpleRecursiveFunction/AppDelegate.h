ea1d02f03f63dcd3d1db622fad0a10f32c8548e1
Steven Shatz <sshatzdeveloper@gmail.com>
2014-10-09 13:49:22 -0400

    Initial Commit

---
+++ SimpleRecursiveFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleRecursiveFunction
+//
+//  Created by Steven Shatz on 10/9/14.
+//  Copyright (c) 2014 Steven Shatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleRecursiveFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleRecursiveFunction
+//
+//  Created by Steven Shatz on 10/9/14.
+//  Copyright (c) 2014 Steven Shatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
